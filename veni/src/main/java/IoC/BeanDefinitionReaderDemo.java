7696b39af511d5b583f4d75435ee15994aae5677
Veni12345 <lity_v@foxmail.com>
2022-07-17 22:01:18 +0800

    BeanDefinitionReaderDemo

---
+++ veni/src/main/java/IoC/BeanDefinitionReaderDemo.java
@@ -0,0 +1,22 @@
+package IoC;
+
+import org.springframework.beans.factory.support.BeanDefinitionReader;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+/**
+ * @author: Veni
+ * @date: 2022/07/17 七月 星期日 8:27
+ * @description:
+ */
+public class BeanDefinitionReaderDemo {
+	BeanDefinitionRegistry registry=new DefaultListableBeanFactory();
+
+	//properties文件
+	BeanDefinitionReader properties=new PropertiesBeanDefinitionReader(registry);
+	//xml文件
+	BeanDefinitionReader xml=new XmlBeanDefinitionReader(registry);
+
+}
