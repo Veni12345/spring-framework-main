9c8171a505301051f9b4a24487bce05df30b7a9d
Veni12345 <lity_v@foxmail.com>
2022-07-17 08:21:53 +0800

    通过直接编码的方式实现Bean的注册与依赖绑定

---
+++ veni/src/main/java/IoC/Library.java
@@ -0,0 +1,21 @@
+package IoC;
+
+import lombok.Data;
+
+/**
+ * @author: Veni
+ * @date: 2022/07/16 七月 星期六 17:32
+ * @description:
+ */
+@Data
+public class Library {
+
+	private Long Id;
+	private String name;
+
+	@Override
+	public String toString(){
+		return "LIBRARY!";
+	}
+
+}
