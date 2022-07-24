94dda0bcd3bb13b65e0a643b9709f4f24678e2e0
Veni12345 <lity_v@foxmail.com>
2022-07-24 11:34:06 +0800

    @AspectJ形式的Introduction 测试编写方法

---
+++ veni/src/main/java/AOP/AspectJ/ICounter.java
@@ -0,0 +1,11 @@
+package AOP.AspectJ;
+
+/**
+ * @author: Veni
+ * @date: 2022/07/24 七月 星期日 11:08
+ * @description: IntroductionAspect 测试接口
+ */
+public interface ICounter {
+	void resetCount();
+	int getCounter();
+}
