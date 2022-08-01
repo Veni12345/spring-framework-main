5ab0ff0d5676de8be0e72e80d580027a547769a2
Veni12345 <lity_v@foxmail.com>
2022-08-01 14:13:32 +0800

    MODIFY

--- veni/src/main/java/IoC/ApplicationContext/ApplicationContextDemo.java
+++ veni/src/main/java/IoC/ApplicationContext/ApplicationContextDemo.java
@@ -1,13 +1,15 @@
 package IoC.ApplicationContext;
 
 
+import IoC.Beans;
 import IoC.Library;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.CommonAnnotationBeanPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-import org.springframework.web.context.support.XmlWebApplicationContext;
+//import org.springframework.web.context.support.XmlWebApplicationContext;
 
 /**
  * @author: Veni
@@ -18,15 +20,20 @@ public class ApplicationContextDemo {
 	public static void main(String[] args) {
 //		ApplicationContext applicationContext1=new XmlWebApplicationContext();
 //		ApplicationContext applicationContext2=new FileSystemXmlApplicationContext();
-		ApplicationContext applicationContext3=new ClassPathXmlApplicationContext("lib.xml");
 
-		Library library = (Library) applicationContext3.getBean("library");
-		System.out.println(library);
+		//1. 通过XML文件 装载Bean配置
+//		ApplicationContext applicationContext3=new ClassPathXmlApplicationContext("lib.xml");
+
+//		Library library = (Library) applicationContext3.getBean("library");
+//		System.out.println(library);
+
+		//2. 通过一个带@Configuration的POJO装载Bean配置
+		ApplicationContext applicationContext4 = new AnnotationConfigApplicationContext(Beans.class);
+		Library library = applicationContext4.getBean("library", Library.class);
+		System.out.println(library.toString());
 
 		BeanPostProcessor commonAnnotationBeanPostProcessor = new CommonAnnotationBeanPostProcessor();
 
-
-		
 
 	}
 }
